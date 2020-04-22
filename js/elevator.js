6efe946621e623b89f4fdeaa06bfeeeb42358333
MolostvovVlad <47096658+MolostvovVlad@users.noreply.github.com>
2020-04-22 17:23:57 +0900

    Add files via upload

---
+++ js/elevator.js
@@ -0,0 +1,9 @@
+let b = document.querySelector(".button");
+let el = document.querySelector(".elevator");
+let pr = document.querySelector(".pr");
+let chek = 0;
+let wind = document.querySelector(".window");      
+let proverka = function() {
+ wind.style.top = "40%";
+};
+ setTimeout(proverka,5000)
---
+++ servis.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/block1.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body><a href="http://covid-19/index.php">
+			<button class="home" style="outline: none;">На главную</button>
+		</a>
+	<div class="head">
+	<h1 class="headtextserv">Сервисы</h1>
+	</div>
+<div class="box">
+	<div class="col-12 row">
+		<div class="col-3">
+			<a href="http://covid-19/servis/provayder.php">
+			<button class="servisblock mt-5" style="outline: none;">
+					<p class="menutext">Провайдеры</p>
+			</button></a>
+			<button class="servisblock mt-5" style="outline: none;">
+					<p class="menutext">Учеба</p>
+			</button>
+			<button class="servisblock mt-5 mb-5" style="outline: none;">
+					<p class="menutext">Магазины</p>
+			</button>
+		</div>
+		<div class="col-1"></div>
+		<div class="col-3">
+			<button class="servisblock mt-5" style="outline: none;">
+					<p class="menutext">Онлайн кино</p>
+			</button>
+			<button class="servisblock mt-5" style="outline: none;">
+					<p class="menutext">Доставка еды</p>
+			</button>
+		</div>
+		<div class="col-1"></div>
+		<div class="col-3">
+			<button class="servisblock mt-5" style="outline: none;">
+					<p class="menutext">Игры</p>
+			</button>
+			<button class="servisblock mt-5" style="outline: none;">
+					<p class="menutext">Комунальные услуги</p>
+			</button>
+		</div>
+		<div class="col-1"></div>
+	</div>
+</div>
+</body>
+</html>
